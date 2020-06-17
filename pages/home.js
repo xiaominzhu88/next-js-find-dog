3a7578fed98d3131da6c413b65bbc0b81bde6b39
xiaomin zhu <xiaomin.zhu88@gmail.com>
2020-06-17 08:20:29 +0200

    Add data fetch and get image, name from random dogs, css in home page

--- pages/home.js
+++ pages/home.js
@@ -4,14 +4,43 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Link from 'next/link';
 
-export default function Home({ data }) {
-  const [dogData, setDogData] = useState(data);
+export default function Home() {
+  const apiKey = process.env.apiKey;
 
-  const fetchData = async () => {
-    const req = await fetch('https://dog.ceo/api/breeds/image/random');
-    const newData = await req.json();
-    return setDogData(newData);
+  const [dogImageUrl, setDogImageUrl] = useState(
+    'https://cdn2.thedogapi.com/images/VSraIEQGd.jpg',
+  );
+  const [dogs, setDogs] = useState([]);
+
+  //if (dogs.length === 0 || !dogs) return <p>No dogs found</p>;
+
+  const fetchData = () => {
+    fetch('https://api.thedogapi.com/v1/images/search', {
+      method: 'GET',
+      dataType: 'JSON',
+      headers: { 'X-Api-Key': `${apiKey}` },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+
+        console.log(result.map((dog) => dog.breeds));
+
+        const newUrl = result.map((a) => a.url);
+        const dogBreeds = result.map((dog) => dog.breeds);
+        console.log(typeof dogName);
+        console.log(dogBreeds[0].map((a) => a.name));
+
+        const dogName = dogBreeds[0].map((a) => a.name);
+
+        setDogImageUrl(newUrl);
+        setDogs(dogName);
+      })
+      .then((error) => {
+        return error;
+      });
   };
+
   function changeImage(e) {
     e.preventDefault();
     fetchData();
@@ -69,12 +98,10 @@ export default function Home({ data }) {
 
       <div className="dogList">
         <div>
-          <img alt={data.message} src={data.message} />
+          <img alt="dog-images" src={dogImageUrl} />
           <button onClick={changeImage}>Change</button>
           <button>Yes</button>
-          {/*<p>Yes</p>*/}
         </div>
-
         <div className={`current pic ${move}`}>
           <img src={pics[index]} alt="cute-dogs" />
           <Link href="/star">
@@ -83,6 +110,9 @@ export default function Home({ data }) {
             </a>
           </Link>
         </div>
+      </div>
+      <div>
+        <p>{dogs}</p>
       </div>
 
       <Footer />
@@ -105,27 +135,34 @@ export default function Home({ data }) {
             letter-spacing: 0.2em;
             line-height: 2em;
             font-family: monospace;
+            font-size: 1.5em;
+            color: #1494cd;
+          }
+          button {
+            margin-bottom: 1em;
+            width: 5em;
+            height: 3em;
+            padding: 5px;
+            border-radius: 5px;
+            cursor: pointer;
+            box-shadow: 2px 9px 11px #1494cd;
+            background-color: rgb(217, 236, 230);
+            font-family: cursive;
+            font-size: 1em;
+            font-weight: bold;
+            outline: none;
+          }
+          button:hover {
+            background-color: yellow;
+            color: red;
+            font-weight: 700;
+          }
+          button:active {
+            transition: transformY(4px);
+            background-color: rgb(235, 208, 121);
           }
         `}
       </style>
     </div>
   );
-}
-
-export async function getServerSideProps(context) {
-  const res = await fetch('https://dog.ceo/api/breeds/image/random');
-
-  //const res = await fetch(
-  //  'https://www.petrescue.com.au/api/listings?states=WA,NSW&interstate=true&////gender=male',
-  //);
-
-  const data = await res.json();
-
-  console.log(data);
-
-  return {
-    props: {
-      data,
-    },
-  };
 }
