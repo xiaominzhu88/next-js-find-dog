b60b5fe1540e9c32c0f908bb171e81952dc0e9c2
xiaomin zhu <xiaomin.zhu88@gmail.com>
2020-07-09 22:53:28 +0200

    Created deleteFavo on DB, created api/deleteFavos which called from contact page onSubmit button

--- pages/api/deleteFavos.ts
+++ pages/api/deleteFavos.ts
@@ -3,7 +3,6 @@ import { deleteFavo } from '../../db';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const favoDogsToDelete = await deleteFavo();
-  //console.log(req.query);
-  // pass fetchedDogs as ARRAY instead object
+
   res.status(200).json(favoDogsToDelete);
 };
